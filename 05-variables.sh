6c5076063c2cb816eafde3995426c0d4ef203eda
shirishakodari <kodarishirisha419@gmail.com>
2026-09-01 19:25:21 +0530

    shell

--- 05-variables.sh
+++ 05-variables.sh
@@ -10,3 +10,9 @@
 echo "enter your password ::" 
 read -s password # read means take input from user ,s means hid passord 
 echo "your password is ::$PASSWORD"  # out put value store in $ 
+
+
+
+# echo "enter your password ::" 
+# read -s password # read means take input from user ,s means hid passord if you want see output after enter remove s
+# echo "your password is ::$PASSWORD"  # out put value store in $ 
--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# this is comment
-NUMBER1=100
-NUMBER2=200
+# # this is comment
+# NUMBER1=100
+# NUMBER2=200
 
-TIMESTAMP=$(date) # $ store the date in timestap variable out give from variable
-echo "Script executed at: $TIMESTAMP"
-SUM=$(($NUMBER1+$NUMBER2)) #first bracket excute cmd get output  store in 2nd bracket
+# TIMESTAMP=$(date) # $ store the date in timestap variable out give from variable
+# echo "Script executed at: $TIMESTAMP" #show output 
+# SUM=$(($NUMBER1+$NUMBER2)) #first bracket excute cmd get output  store in 2nd bracket
 
-echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
+# echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
+
+
+
+
+#VARIABLE=$(COMMAND)->excute command and take output store variable 
+
+NAME="SHIRISHA"
+AGE="25"
+
+TIMESTAMP=$(date)
+echo "excute timestamp at: $TIMESTAMP"

d56a1bc61add78ee5bd9c696ee23c79f20549ca4
shirishakodari <kodarishirisha419@gmail.com>
2026-09-02 14:12:21 +0530

    shell

--- 21-git-tag.sh
+++ 21-git-tag.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "create tag for release"
+# echo "create tag for release"
+
+ls /tmp
+
+STATUS=$?
+
+if [ $STATUS -eq 0 ]
+then
+    echo "Command successful"
+else
+    echo "Command failed"
+fi

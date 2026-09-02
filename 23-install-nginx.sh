3bb366b3a986c9e9da021bc58773223d52e088e0
shirishakodari <kodarishirisha419@gmail.com>
2026-09-02 13:33:42 +0530

    shell

--- 23-install-nginx.sh
+++ 23-install-nginx.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-
+LOGFILE="nginx.log" #create log file and store logs in nginx.log  
 echo "install nginx ..successfully"
 
- sudo dnf install nginx -y 
+ sudo dnf install nginx -y > "$LOGFILE" 2>&1  #> append content in file and overite 
 
 if [ $? -eq 0 ] #0-->success and 1,2 -->failed 
  then 
  echo "install nginx successfully" 
- sudo systemctl start nginx
- sudo systemctl enable nginx
+ sudo systemctl start nginx > "$LOGFILE" 2>&1
+ sudo systemctl enable nginx > "$LOGFILE" 2>&1
 
 echo "Nginx is running"
 else
+
+  echo "Nginx installation failed"
+  echo "Check $LOGFILE for details"
+  
  exit 1 
 
 fi 

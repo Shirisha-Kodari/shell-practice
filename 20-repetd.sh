51f0c751985198b3b68f85760e219e1ee86ac695
shirishakodari <kodarishirisha419@gmail.com>
2026-09-02 14:17:26 +0530

    shell

--- 20-repetd.sh
+++ 20-repetd.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 
-str="missap"
 
-echo "$str" | grep -o . | sort | uniq -cd
+LOGFILE="old_logs.txt"
+
+find /var/log -type f -mtime 4 > "$LOGFILE"
+
+echo "4 days old logs stored successfully"
 
 # str="missap"
 
-# echo "$str" \
-#   | grep -o . \        # split into individual characters
-#   | sort \             # sort alphabetically
-#   | uniq -c            # count repeating letters
+# echo "$str" | grep -o . | sort | uniq -cd
+
+# # str="missap"
+
+# # echo "$str" \
+# #   | grep -o . \        # split into individual characters
+# #   | sort \             # sort alphabetically
+# #   | uniq -c            # count repeating letters

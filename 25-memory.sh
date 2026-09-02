be9e1e0c694692c44182f10285908951b724df4d
shirishakodari <kodarishirisha419@gmail.com>
2026-09-02 14:52:15 +0530

    shell

--- 25-memory.sh
+++ 25-memory.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 
-echo "CPU Usage:"
-top -bn1 | grep "Cpu(s)"
+# echo "CPU Usage:"
+# top -bn1 | grep "Cpu(s)"
 
-echo "Memory Usage:"
-free -h
+# echo "Memory Usage:"
+# free -h
+
+
+
+CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100-$8}')
+MEMORY=$(free | awk '/Mem:/ {print $3/$2*100}')
+
+echo "CPU Usage: $CPU%"
+echo "Memory Usage: $MEMORY%"
